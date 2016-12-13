c1d29047f45082a5d190da50fb407bc6c208fded
dandi <deliterjob@gmail.com>
2016-12-13 18:30:29 +0100

    fixes

--- src/starter/Main.java
+++ src/starter/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         ArticlesController.getInstance().sendReview("review di prova", "gonna lunga", "simone@gmail.com", 3, "simone@gmail.com");
+        ArticlesController.getInstance().sendWarning("review di prova", "gonna lunga", "simone@gmail.com", "simone@gmail.com");
     }
 }
